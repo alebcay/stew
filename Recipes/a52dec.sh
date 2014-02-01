5d9e1baa9756a47685b1e08f041650a2b6a10bea
Caleb Xu <calebcenter@live.com>
2014-02-01 15:26:18 -0500

    Still working on the download system

--- Recipes/a52dec.sh
+++ Recipes/a52dec.sh
@@ -1,9 +1,9 @@
 [ -d "$PKGNAME" ] && rm -rf "$PKGNAME"
 mkdir "$PKGNAME"
 cd "$PKGNAME"
-curl -Lks http://liba52.sourceforge.net/files/a52dec-0.7.4.tar.gz > a52dec.tar.gz
-tar xz a52dec.tar.gz
-cd a52dec-0.7.4.tar.gz
+curl -LkOs http://liba52.sourceforge.net/files/a52dec-0.7.4.tar.gz
+tar xzvf a52dec-0.7.4.tar.gz
+cd a52dec-0.7.4
 CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
 ./configure --disable-debug --disable-dependency-tracking --prefix="$STEWDIR"/Bowls/a52dec --enable-shared
 make -j $CORES
