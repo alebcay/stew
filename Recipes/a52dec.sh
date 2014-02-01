5ed4f671026d72d7a8849bbbf3f0c79ca6f9d9f2
Caleb Xu <calebcenter@live.com>
2014-02-01 16:03:59 -0500

    Still testing removal feature

--- Recipes/a52dec.sh
+++ Recipes/a52dec.sh
@@ -10,12 +10,12 @@ cd "$PKGNAME"
 echo -e "\033[1;34m--> Downloading source package: http://liba52.sourceforge.net/files/a52dec-0.7.4.tar.gz\033[22;0m"
 curl -LkOs http://liba52.sourceforge.net/files/a52dec-0.7.4.tar.gz
 echo -e "\033[1;34m--> Opening package\033[22;0m"
-tar xzvf a52dec-0.7.4.tar.gz >/dev/null 2>&1
+tar xzvf a52dec-0.7.4.tar.gz
 cd a52dec-0.7.4
 CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
 echo -e "\033[1;34m--> Building $PKGNAME\033[22;0m"
-./configure --disable-debug --disable-dependency-tracking --prefix="$STEWDIR"/Bowls/a52dec --enable-shared >/dev/null 2>&1
-make -j $CORES >/dev/null 2>&1
-make install >/dev/null 2>&1
-ln -s "$STEWDIR"/Bowls/a52dec/bin/a52dec /usr/local/bin/a52dec >/dev/null 2>&1
-ln -s "$STEWDIR"/Bowls/a52dec/bin/extract_a52 /usr/local/bin/extract_a52 >/dev/null 2>&1
+./configure --disable-debug --disable-dependency-tracking --prefix="$STEWDIR"/Bowls/a52dec --enable-shared
+make -j $CORES
+make install
+ln -s "$STEWDIR"/Bowls/a52dec/bin/a52dec /usr/local/bin/a52dec
+ln -s "$STEWDIR"/Bowls/a52dec/bin/extract_a52 /usr/local/bin/extract_a52
