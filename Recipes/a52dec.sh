80be0ab16896b925887d3fc7034fa304a1a24f04
Caleb Xu <calebcenter@live.com>
2014-02-01 15:38:04 -0500

    Still testing a52dec

--- Recipes/a52dec.sh
+++ Recipes/a52dec.sh
@@ -1,11 +1,14 @@
 [ -d "$PKGNAME" ] && rm -rf "$PKGNAME"
 mkdir "$PKGNAME"
 cd "$PKGNAME"
+echo -e "\033[1;34m--> Downloading source package: http://liba52.sourceforge.net/files/a52dec-0.7.4.tar.gz\033[22;0m"
 curl -LkOs http://liba52.sourceforge.net/files/a52dec-0.7.4.tar.gz
-tar xzvf a52dec-0.7.4.tar.gz
+echo -e "\033[1;34m--> Opening package\033[22;0m"
+tar xzvf a52dec-0.7.4.tar.gz >/dev/null 2>&1
 cd a52dec-0.7.4
 CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
-./configure --disable-debug --disable-dependency-tracking --prefix="$STEWDIR"/Bowls/a52dec --enable-shared
-make -j $CORES
-make install
+echo -e "\033[1;34m--> Building $PKGNAME\033[22;0m"
+./configure --disable-debug --disable-dependency-tracking --prefix="$STEWDIR"/Bowls/a52dec --enable-shared 2>&1
+make -j $CORES 2>&1
+make install 2>&1
 ln -s "$STEWDIR"/Bowls/a52dec/bin/a52dec /usr/local/bin/a52dec
