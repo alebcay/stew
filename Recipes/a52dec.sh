3964c8c96a468f6e8b7ba38dc5a8666d5cf771e5
Caleb Xu <calebcenter@live.com>
2014-02-01 15:40:41 -0500

    Still testing a52dec

--- Recipes/a52dec.sh
+++ Recipes/a52dec.sh
@@ -8,7 +8,8 @@ tar xzvf a52dec-0.7.4.tar.gz >/dev/null 2>&1
 cd a52dec-0.7.4
 CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
 echo -e "\033[1;34m--> Building $PKGNAME\033[22;0m"
-./configure --disable-debug --disable-dependency-tracking --prefix="$STEWDIR"/Bowls/a52dec --enable-shared 2>&1
+./configure --disable-debug --disable-dependency-tracking --prefix="$STEWDIR"/Bowls/a52dec --enable-shared >/dev/null 2>&1
 make -j $CORES 2>&1
 make install 2>&1
-ln -s "$STEWDIR"/Bowls/a52dec/bin/a52dec /usr/local/bin/a52dec
+ln -s "$STEWDIR"/Bowls/a52dec/bin/a52dec /usr/local/bin/a52dec >/dev/null 2>&1
+ln -s "$STEWDIR"/Bowls/a52dec/bin/extract_a52 /usr/local/bin/extract_a52 >/dev/null 2>&1
