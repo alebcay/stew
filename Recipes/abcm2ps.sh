26948443ebc5fcbcfdcef37a8031a1c4c4be1909
Caleb Xu <calebcenter@live.com>
2014-02-02 14:07:48 -0500

    Fixed issue with abcm2ps

--- Recipes/abcm2ps.sh
+++ Recipes/abcm2ps.sh
@@ -4,10 +4,10 @@ cd "$PKGNAME"
 echo -e "\033[1;34m--> Downloading source package: http://moinejf.free.fr/abcm2ps-6.6.22.tar.gz\033[22;0m"
 curl -LkOs http://moinejf.free.fr/abcm2ps-6.6.22.tar.gz
 echo -e "\033[1;34m--> Opening package\033[22;0m"
-tar xzvf abcm2ps-6.6.22.tar.gz  >/dev/null 2>&1
+tar xzvf abcm2ps-6.6.22.tar.gz >/dev/null 2>&1
 cd abcm2ps-6.6.22
 CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
 echo -e "\033[1;34m--> Building $PKGNAME\033[22;0m"
-./configure --disable-debug --disable-dependency-tracking --prefix="$STEWDIR"/Bowls/abcm2ps --enable-shared
-make -j $CORES install
-ln -s "$STEWDIR"/Bowls/abcm2ps/bin/aacgain /usr/local/bin/abcm2ps
+./configure --disable-debug --disable-dependency-tracking --prefix="$STEWDIR"/Bowls/abcm2ps --enable-shared >/dev/null 2>&1
+make -j $CORES install >/dev/null 2>&1
+ln -s "$STEWDIR"/Bowls/abcm2ps/bin/aacgain /usr/local/bin/abcm2ps >/dev/null 2>&1
