b66a58cb6f3730e9a59695948b29713c065eb36a
Caleb Xu <calebcenter@live.com>
2014-02-02 15:32:05 -0500

    Added ack

---
+++ Recipes/ack.sh
@@ -0,0 +1,10 @@
+ [ -a "/usr/local/bin/ack" ] && echo -e "\033[1;34m--> Package already configured, upgrading instead of installing\033[22;0m" && rm /usr/local/bin/ack
+ [ -d "$PKGNAME" ] || mkdir "$PKGNAME"
+ cd "$PKGNAME"
+ echo -e "\033[1;34m--> Downloading source package: http://beyondgrep.com/ack-2.12-single-file\033[22;0m"
+ curl -Os http://beyondgrep.com/ack-2.12-single-file
+ echo -e "\033[1;34m--> Opening package\033[22;0m"
+ cp ack-2.12-single-file "$STEWDIR"/Bowls/ack/ack >/dev/null 2>&1
+ echo -e "\033[1;34m--> Building $PKGNAME\033[22;0m"
+ chmod 0755 "$STEWDIR"/Bowls/ack/bin/ack >/dev/null 2>&1
+ ln -s "$STEWDIR"/Bowls/ack/bin/ack /usr/local/bin/ack >/dev/null 2>&1
