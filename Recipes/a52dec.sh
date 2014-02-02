62489f66718f1eae1401bdbb1d1963c2c1707f2b
Caleb Xu <calebcenter@live.com>
2014-02-02 08:04:34 -0500

    Allow for packages to be upgraded

--- Recipes/a52dec.sh
+++ Recipes/a52dec.sh
@@ -1,15 +1,15 @@
-[ -d "$PKGNAME" ] && rm -rf "$PKGNAME"
-mkdir "$PKGNAME"
+[ -a "/usr/local/bin/a52dec" ] && echo -e "\033[1;34m--> Package already configured, upgrading instead of installing\033[22;0m" && rm /usr/local/bin/a52dec
+[ -a "/usr/local/bin/extract_a52" ] && rm /usr/local/bin/extract_a52
+[ -d "$PKGNAME" ] || mkdir "$PKGNAME"
 cd "$PKGNAME"
 echo -e "\033[1;34m--> Downloading source package: http://liba52.sourceforge.net/files/a52dec-0.7.4.tar.gz\033[22;0m"
 curl -LkOs http://liba52.sourceforge.net/files/a52dec-0.7.4.tar.gz
 echo -e "\033[1;34m--> Opening package\033[22;0m"
-tar xzvf a52dec-0.7.4.tar.gz
+tar xzvf a52dec-0.7.4.tar.gz >/dev/null 2>&1
 cd a52dec-0.7.4
 CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
 echo -e "\033[1;34m--> Building $PKGNAME\033[22;0m"
-./configure --disable-debug --disable-dependency-tracking --prefix="$STEWDIR"/Bowls/a52dec --enable-shared
-make -j $CORES
-make install
-ln -s "$STEWDIR"/Bowls/a52dec/bin/a52dec /usr/local/bin/a52dec
-ln -s "$STEWDIR"/Bowls/a52dec/bin/extract_a52 /usr/local/bin/extract_a52
+./configure --disable-debug --disable-dependency-tracking --prefix="$STEWDIR"/Bowls/a52dec --enable-shared >/dev/null 2>&1
+make -j $CORES install >/dev/null 2>&1
+ln -s "$STEWDIR"/Bowls/a52dec/bin/a52dec /usr/local/bin/a52dec >/dev/null 2>&1
+ln -s "$STEWDIR"/Bowls/a52dec/bin/extract_a52 /usr/local/bin/extract_a52 >/dev/null 2>&1
--- Recipes/aacgain.sh
+++ Recipes/aacgain.sh
@@ -1,13 +1,13 @@
-[ -d "$PKGNAME" ] && rm -rf "$PKGNAME"
-mkdir "$PKGNAME"
+[ -a "/usr/local/bin/aacgain" ] && echo -e "\033[1;34m--> Package already configured, upgrading instead of installing\033[22;0m" && rm /usr/local/bin/aacgain
+[ -d "$PKGNAME" ] || mkdir "$PKGNAME"
 cd "$PKGNAME"
 echo -e "\033[1;34m--> Downloading source package: http://aacgain.altosdesign.com/alvarez/aacgain-1.8.tar.bz2\033[22;0m"
 curl -LkOs http://aacgain.altosdesign.com/alvarez/aacgain-1.8.tar.bz2
 echo -e "\033[1;34m--> Opening package\033[22;0m"
-tar xjvf aacgain-1.8.tar.bz2
+tar xjvf aacgain-1.8.tar.bz2 >/dev/null 2>&1
 cd aacgain-1.8
 CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
 echo -e "\033[1;34m--> Building $PKGNAME\033[22;0m"
-./configure --disable-debug --disable-dependency-tracking --prefix="$STEWDIR"/Bowls/aacgain --enable-shared
-make -j $CORES install
-ln -s "$STEWDIR"/Bowls/aacgain/bin/aacgain /usr/local/bin/aacgain
+./configure --disable-debug --disable-dependency-tracking --prefix="$STEWDIR"/Bowls/aacgain --enable-shared >/dev/null 2>&1
+make -j $CORES install >/dev/null 2>&1
+ln -s "$STEWDIR"/Bowls/aacgain/bin/aacgain /usr/local/bin/aacgain >/dev/null 2>&1
