5b40075c81806860ff4965d5ed02cd2d01ce118a
Caleb Xu <calebcenter@live.com>
2014-02-02 15:35:28 -0500

    Fixing ack

--- Recipes/ack.sh
+++ Recipes/ack.sh
@@ -4,7 +4,10 @@
  echo -e "\033[1;34m--> Downloading source package: http://beyondgrep.com/ack-2.12-single-file\033[22;0m"
  curl -Os http://beyondgrep.com/ack-2.12-single-file
  echo -e "\033[1;34m--> Opening package\033[22;0m"
- cp ack-2.12-single-file "$STEWDIR"/Bowls/ack/ack
+ [ -d "$STEWDIR"/Bowls ] || mkdir "$STEWDIR"/Bowls
+ [ -d "$STEWDIR"/Bowls/ack ] || mkdir "$STEWDIR"/Bowls/ack
+ [ -d "$STEWDIR"/Bowls/ack/bin ] || mkdir "$STEWDIR"/Bowls/ack/bin
+ cp ack-2.12-single-file "$STEWDIR"/Bowls/ack/bin/ack
  echo -e "\033[1;34m--> Building $PKGNAME\033[22;0m"
  chmod 0755 "$STEWDIR"/Bowls/ack/bin/ack
  ln -s "$STEWDIR"/Bowls/ack/bin/ack /usr/local/bin/ack
