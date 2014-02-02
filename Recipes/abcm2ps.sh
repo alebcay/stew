0747414f1a9cb91cd2b3fdd24292db847525c503
Caleb Xu <calebcenter@live.com>
2014-02-02 08:38:14 -0500

    Fixed issue on abcm2ps

--- Recipes/abcm2ps.sh
+++ Recipes/abcm2ps.sh
@@ -4,8 +4,8 @@ cd "$PKGNAME"
 echo -e "\033[1;34m--> Downloading source package: http://moinejf.free.fr/abcm2ps-6.6.22.tar.gz\033[22;0m"
 curl -LkOs http://moinejf.free.fr/abcm2ps-6.6.22.tar.gz
 echo -e "\033[1;34m--> Opening package\033[22;0m"
-tar xjvf aacgain-1.8.tar.bz2 >/dev/null 2>&1
-cd aacgain-1.8
+tar xjvf abcm2ps-6.6.22.tar.gz >/dev/null 2>&1
+cd abcm2ps-6.6.22.tar.gz
 CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
 echo -e "\033[1;34m--> Building $PKGNAME\033[22;0m"
 ./configure --disable-debug --disable-dependency-tracking --prefix="$STEWDIR"/Bowls/aacgain --enable-shared >/dev/null 2>&1
